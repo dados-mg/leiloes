61b6b7516113168f45404810b0df48bd6eb5e19e
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-06-20 14:46:07 +0200

    Remove white spaces from pandas string
    Fix #1
    See https://github.com/dados-mg/leiloes/issues/1#issuecomment-1598703608

--- scripts/convert_csv.py
+++ scripts/convert_csv.py
@@ -13,8 +13,13 @@ def convert_csv():
     for column in columns:
       new_columns.append(snake_small_case(column))
     csv_name = file.split('.xlsx')[0]
-    #import ipdb; ipdb.set_trace(context=10)
     read_file.columns=new_columns
+
+    # Remove white spaces from the beginning and the end of all columns
+    for column in new_columns:
+        if read_file[column].dtype == object and isinstance(read_file.iloc[0][column], str):
+            read_file[column] = [read_file[column][i].strip() for i in range(0, len(read_file))]
+
     read_file.to_csv (f'data/{csv_name}.csv', index = None, header=True, sep = ';', decimal = ',', encoding = 'utf-8-sig', na_rep = "")
 
 def snake_small_case(column):
